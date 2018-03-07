b7b080efe0bb46843ebe21a4a7f2bbcffa2ec82c
Marina Balioura <mettta@gmail.com>
2018-03-07 09:02:59 +0100

    inline code...

--- test/test.js
+++ test/test.js
@@ -10,17 +10,3 @@ describe("Tree", function() {
 
 	});
 });
-
-
-
-
-// function makeTest(x) {
-// 	var expected = x * x * x;
-// 	it("при возведении " + x + " в степень 3 результат: " + expected, function() {
-// 		assert.equal(pow(x, 3), expected);
-// 	});
-// 	}
-
-// 	for (var x = 1; x <= 5; x++) {
-// 	makeTest(x);
-// }
