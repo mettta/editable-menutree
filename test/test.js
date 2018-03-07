c7aa5dacc2460768f9e16af52deba5b96b372468
Marina Balioura <mettta@gmail.com>
2018-03-07 09:31:23 +0100

    Add 'defer'
    Modules are defer by default. Regular inline scripts ignore defer ... but inline module scripts are deferred.
    https://github.com/mochajs/mocha/issues/2816

--- test/test.js
+++ test/test.js
@@ -1,3 +1,20 @@
+'use strict';
+
+// var assert = chai.assert;
+
+import {Tree} from './../src/components/tree/tree.js';
+
+const tree = new Tree({
+	data: []
+});
+
+// const expect = chai.expect;
+
+// describe('test', function() {
+//     it('hypotenuse 5, 12', function() { expect(MyLib.hypotenuse(5, 12)).to.equal(13); });
+//     it('hypotenuse 8, 15', function() { expect(MyLib.hypotenuse(8, 15)).to.equal(17); });
+// });
+
 describe("Tree", function() {
 
 	before(function() {  });
@@ -5,7 +22,7 @@ describe("Tree", function() {
 	describe("Tree.getNodeIdx", function() {
 
 		it("Для массива индексов [1,2,3] возвращает последний - 3", function() {
-			assert.equal(tree.getNodeIdx([1,2,3]), 3);
+			chai.assert.equal(tree.getNodeIdx([1,2,3]), 3);
 		});
 
 	});
