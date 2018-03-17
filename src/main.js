4a16c81a486cee76052050a6eb53ba7d53a191ed
Marina Balioura <mettta@gmail.com>
2018-03-17 15:25:38 +0100

    Add Firebase as npm package.

--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ import './main.css';
 const el = document.createElement('div');
 el.classList.add('js-app');
 
-window.onload = function() {
-	document.body.append(el);
-}
-
 new App({
 	el: el
 });
 
+window.onload = function() {
+	document.body.append(el);
+}
+
 // Initialize Firebase
+
+import * as firebase from 'firebase';
+
 var config = {
 	apiKey: "AIzaSyA6MrUgAlk-MI9ALoS0wDrBGihokS1J-EQ",
 	authDomain: "editable-tree.firebaseapp.com",
@@ -21,7 +24,8 @@ var config = {
 	projectId: "editable-tree",
 	storageBucket: "",
 	messagingSenderId: "1087544042407"
-	};
+};
+
 firebase.initializeApp(config);
 
 let TESTDATA = firebase.database().ref();
